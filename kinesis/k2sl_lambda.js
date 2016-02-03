b4db2700907c943ce68e91a7c0215c7a1e6b9b5c
d-smith <doug.smith@fmr.com>
2016-02-03 14:57:23 -0800

    Added lambda function to write kinesis data to Sumo

---
+++ kinesis/k2sl_lambda.js
@@ -0,0 +1,34 @@
+var https = require('https');
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//      // Remember to change the hostname and path to match your collection API and specific HTTP-source endpoint
+//              // See more at: https://service.sumologic.com/help/Default.htm#Collector_Management_API.htm
+//                      ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+var options = { 'hostname': 'endpoint1.collection.us2.sumologic.com',
+  'path': 'https://endpoint1.collection.us2.sumologic.com/receiver/v1/http/XXXXX',
+  'method': 'POST'
+};
+
+exports.handler = function(event, context) {
+
+  var req = https.request(options, function(res) {
+      var body = '';
+      console.log('Status:', res.statusCode);
+      res.setEncoding('utf8');
+      res.on('data', function(chunk) { body += chunk; });
+      res.on('end', function() {
+        console.log('Successfully processed HTTPS response');
+        context.succeed(); });
+    });
+
+  options.agent = new https.Agent(options);
+
+  req.on('error', context.fail);
+
+  event.Records.forEach(function(record) {
+    var payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
+    req.write(payload + '\n');
+  })
+  req.end();
+
+}
