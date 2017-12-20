1a194b8adf27eb2b6e7926dbf7c061f2ef3fab44
Himanshu Pal <hpal@sumologic.com>
2017-12-20 19:35:25 +0530

    utils library created

--- dlq_processor/DLQProcessor.js
+++ dlq_processor/DLQProcessor.js
@@ -1,77 +1,38 @@
 var AWS = require("aws-sdk");
 var processLogsHandler = require('./cloudwatchlogs_lambda').processLogs;
+var DLQUtils = require("./sumo-dlq-function-utils");
+var Messages = new DLQUtils.Messages(process.env);
+var invokeLambdas = DLQUtils.invokeLambdas;
 
-function receiveMessages(messageCount, sqs, env, callback) {
-    var params = {
-        QueueUrl: env.TASK_QUEUE_URL,
-        MaxNumberOfMessages: messageCount
-    };
-    sqs.receiveMessage(params, function (err, data) {
-        if (err) {
-            console.error(err, err.stack);
-            callback(err);
-        } else {
-            callback(null, data.Messages);
-        }
-    });
-}
-
-function deleteMessage(sqs, env, receiptHandle, cb) {
-    sqs.deleteMessage({
-        ReceiptHandle: receiptHandle,
-        QueueUrl: env.TASK_QUEUE_URL
-    }, cb);
-}
-
-function initworkers(env, context) {
-
-    for (var i = 0; i < parseInt(env.NUM_OF_WORKERS); i++) {
-        var lambda = new AWS.Lambda({
-          region: env.AWS_REGION
-        });
-        lambda.invoke({
-            InvocationType: 'Event',
-            FunctionName: context.functionName,
-            Payload: '{"is_worker": "1"}'
-        }, function(err, data) {
-           if (err) {
-               context.fail(err);
-           } else {
-               context.succeed('success');
-           }
-        });
-    }
-}
 exports.consumeMessages = function (env, context, callback) {
     var sqs = new AWS.SQS({region: env.AWS_REGION});
 
-    receiveMessages(10, sqs, env, function (err, messages) {
+    Messages.receiveMessages(10, function (err, messages) {
 
         if (err) {
             callback(err);
         } else if (messages && messages.length > 0) {
-            env['SUMO_CLIENT_HEADER'] = 'dlq-cwl-lambda';
-            var fail_cnt = 0;
+            var fail_cnt = 0, payload = '{"is_worker": "1"}';
             console.log("Messages Received", messages.length);
             for (var i = 0; i < messages.length; i++) {
                 try {
                     var logdata = JSON.parse(messages[i].Body).awslogs.data;
                     processLogsHandler(env, context, logdata);
-                    deleteMessage(sqs, env, messages[i].ReceiptHandle, callback);
+                    Messages.deleteMessage(messages[i].ReceiptHandle, callback);
                 } catch(err) {
                     fail_cnt += 1;
                 }
             }
             if (fail_cnt == 0 && (parseInt(env.is_worker) === 0)) {
-                initworkers(env, context);
+                invokeLambdas(env.AWS_REGION, parseInt(env.NUM_OF_WORKERS),
+                              context.functionName, payload, context);
             }
-            callback(null, fail_cnt + ' success');
+            callback(null, (messages.length-fail_cnt) + ' success');
         } else {
             callback(null, 'success');
         }
     });
 };
-exports.AWS = AWS;
 
 exports.handler = function (event, context, callback) {
 
---
+++ dlq_processor/sumo-dlq-function-utils/lib/DLQUtils.js
@@ -0,0 +1,52 @@
+var AWS = require("aws-sdk");
+
+function Messages(env) {
+    this.sqs = new AWS.SQS({region: env.AWS_REGION});
+    this.env = env;
+}
+
+Messages.prototype.receiveMessages = function (messageCount, callback) {
+    var params = {
+        QueueUrl: this.env.TASK_QUEUE_URL,
+        MaxNumberOfMessages: messageCount
+    };
+    this.sqs.receiveMessage(params, function (err, data) {
+        if (err) {
+            console.error(err, err.stack);
+            callback(err);
+        } else {
+            callback(null, data.Messages);
+        }
+    });
+};
+
+Messages.prototype.deleteMessage = function (receiptHandle, callback) {
+    this.sqs.deleteMessage({
+        ReceiptHandle: receiptHandle,
+        QueueUrl: this.env.TASK_QUEUE_URL
+    }, callback);
+};
+
+function invokeLambdas(awsRegion, numOfWorkers, functionName, payload, context) {
+
+    for (var i = 0; i < numOfWorkers; i++) {
+        var lambda = new AWS.Lambda({
+          region: awsRegion
+        });
+        lambda.invoke({
+            InvocationType: 'Event',
+            FunctionName: functionName,
+            Payload: payload
+        }, function(err, data) {
+           if (err) {
+               context.fail(err);
+           } else {
+               context.succeed('success');
+           }
+        });
+    }
+}
+module.exports = {
+    Messages: Messages,
+    invokeLambdas: invokeLambdas
+}
