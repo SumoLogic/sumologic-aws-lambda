cf7fa4f5ef5fb86ebb5140f4741e852e405fe2c7
Himanshu Pal <hp.iiita@gmail.com>
2018-04-05 17:10:21 +0530

    added prod deploy, replaced email and swapped 1) & 2) behavior

--- dlq_processor/cloudwatchlogs_lambda.js
+++ dlq_processor/cloudwatchlogs_lambda.js
@@ -33,9 +33,9 @@ function getConfig(env, errorHandler) {
 
         // The following parameters override the sourceCategoryOverride, sourceHostOverride and sourceNameOverride metadata fields within SumoLogic.
         // Not these can also be overridden via json within the message payload. See the README for more information.
-        "sourceCategoryOverride": ("SOURCE_CATEGORY_OVERRIDE" in env) ?  env.SOURCE_CATEGORY_OVERRIDE: 'none',  // If none sourceCategoryOverride will not be overridden
-        "sourceHostOverride": ("SOURCE_HOST_OVERRIDE" in env) ? env.SOURCE_HOST_OVERRIDE : 'none',          // If none sourceHostOverride will not be set to the name of the logGroup
-        "sourceNameOverride": ("SOURCE_NAME_OVERRIDE" in env) ? env.SOURCE_NAME_OVERRIDE : 'none',          // If none sourceNameOverride will not be set to the name of the logStream
+        "sourceCategoryOverride": ("SOURCE_CATEGORY_OVERRIDE" in env) ?  env.SOURCE_CATEGORY_OVERRIDE: '',  // If none sourceCategoryOverride will not be overridden
+        "sourceHostOverride": ("SOURCE_HOST_OVERRIDE" in env) ? env.SOURCE_HOST_OVERRIDE : '',          // If none sourceHostOverride will not be set to the name of the logGroup
+        "sourceNameOverride": ("SOURCE_NAME_OVERRIDE" in env) ? env.SOURCE_NAME_OVERRIDE : '',          // If none sourceNameOverride will not be set to the name of the logStream
         "SUMO_CLIENT_HEADER": env.SUMO_CLIENT_HEADER || 'cwl-aws-lambda',
         // CloudWatch logs encoding
         "encoding": env.ENCODING || 'utf-8'  // default is utf-8
@@ -137,7 +137,7 @@ exports.processLogs = function (env, eventAwslogsData, errorHandler) {
             };
             // removing headers with 'none'
             for (var key in headers) {
-                if (headers[key] === 'none')
+                if (!headers[key] || (headers[key].toLowerCase() === 'none'))
                     delete headers[key]
             }
             options.headers = headers
