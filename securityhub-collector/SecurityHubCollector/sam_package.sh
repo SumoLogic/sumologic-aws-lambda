11845b6ea732a2ae32842eca2292a82680e883b3
himanshu219 <hp.iiita@gmail.com>
2018-12-05 19:25:56 +0530

    stringent permissions and removed account id

--- securityhub-collector/SecurityHubCollector/sam_package.sh
+++ securityhub-collector/SecurityHubCollector/sam_package.sh
@@ -28,4 +28,4 @@ sam package --template-file template.yaml --s3-bucket $SAM_S3_BUCKET  --output-t
 sam deploy --template-file packaged.yaml --stack-name testingsecurityhubcollector --capabilities CAPABILITY_IAM --region $AWS_REGION --parameter-overrides S3SourceBucketName=securityhubfindings
 #aws cloudformation describe-stack-events --stack-name testingsecurityhublambda --region $AWS_REGION
 #aws cloudformation get-template --stack-name testingsecurityhublambda  --region $AWS_REGION
-# aws serverlessrepo create-application-version --region us-east-1 --application-id arn:aws:serverlessrepo:us-east-1:956882708938:applications/sumologic-securityhub-connector --semantic-version 1.0.1 --template-body file://packaged.yaml
+# aws serverlessrepo create-application-version --region us-east-1 --application-id arn:aws:serverlessrepo:us-east-1:$AWS_ACCOUNT_ID:applications/sumologic-securityhub-connector --semantic-version 1.0.1 --template-body file://packaged.yaml
