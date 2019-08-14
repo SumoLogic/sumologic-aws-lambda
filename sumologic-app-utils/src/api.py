181ded9069187975f663d641a100af7864283b73
himanshu219 <hp.iiita@gmail.com>
2019-08-14 10:54:35 +0530

    SUMO-116297: app install fixed for SLF trial account type

--- sumologic-app-utils/src/api.py
+++ sumologic-app-utils/src/api.py
@@ -65,12 +65,17 @@ class Resource(object):
             return 'https://%s-api.sumologic.net/api' % self.deployment
 
     def is_enterprise_or_trial_account(self):
-        to_time = int(time.time())*100
+        to_time = int(time.time())*1000
         from_time = to_time - 5*60*1000
         try:
-            response = self.sumologic_cli.search_job("_sourceCategory=*Guardduty", fromTime=from_time, toTime=to_time)
+            response = self.sumologic_cli.search_job("benchmarkcat guardduty", fromTime=from_time, toTime=to_time)
+            print("schedule job status: %s" % response)
+            response = self.sumologic_cli.search_job_status(response)
             print("job status: %s" % response)
-            return True
+            if len(response.get("pendingErrors", [])) > 0:
+                return False
+            else:
+                return True
         except Exception as e:
             if hasattr(e, "response") and e.response.status_code == 403:
                 return False
@@ -399,9 +404,13 @@ class App(Resource):
 if __name__ == '__main__':
 
     params = {
-        "access_id": "",
-        "access_key": "",
-        "deployment": ""
+        # "access_id": "su7SgzaX1GEFj1",
+        # "access_key": "IPBdLkE64u6tgeGzxM3UkpVRghfvFhbkhD3koufHNj2SGIy70NqOIL6GS7f88gfz",
+        # "deployment": "us2"
+        "access_id": "suNNLllvfjDK4s",
+        "access_key": "lbCmtyd09TcK0uSZX7WmIhDuwBqIKs5U1FvJ8Q5TFYkdWNodhVQYtntIPq4GhMuX",
+        "deployment": "us1"
+
     }
     collector_id = None
     collector_type = "Hosted"
