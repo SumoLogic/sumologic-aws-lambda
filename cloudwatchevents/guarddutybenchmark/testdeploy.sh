19aa6c9ce9b58df51451ef5ae9abb0c2073b3e0d
himanshu219 <hp.iiita@gmail.com>
2019-12-16 15:45:48 +0530

    deploy file for gd benchmark

---
+++ cloudwatchevents/guarddutybenchmark/testdeploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$AWS_PROFILE" == "prod" ]
+then
+    SAM_S3_BUCKET="appdevstore"
+    AWS_REGION="us-east-1"
+else
+    SAM_S3_BUCKET="cf-templates-5d0x5unchag-us-east-2"
+    AWS_REGION="us-east-2"
+fi
+
+version="1.0.8"
+
+sam package --template-file template_v2.yaml --s3-bucket $SAM_S3_BUCKET  --output-template-file packaged_v2.yaml --s3-prefix "guarddutybenchmark/v$version"
+
+sam publish --template packaged_v2.yaml --region $AWS_REGION --semantic-version $version
+
+
+ACCESS_ID=""
+ACCESS_KEY=""
+SUMO_DEPLOYMENT="us2"
+
+# sam deploy --template-file packaged_v2.yaml --stack-name testinggdbenchmarknew --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region $AWS_REGION --parameter-overrides SumoDeployment="$SUMO_DEPLOYMENT" SumoAccessID="$ACCESS_ID" SumoAccessKey="$ACCESS_KEY" RemoveSumoResourcesOnDeleteStack="true"
+
