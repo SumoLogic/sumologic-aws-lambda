07d39de4617a609578cb4a56bf6a89c69c1252ec
sourabh <sourabh@sumologic.com>
2020-01-16 23:32:20 +0530

    Changing folder to templates resources.

---
+++ sumologic-aws-observability/SumoLogicAWSObservabilityHelper/zip.sh
@@ -0,0 +1,16 @@
+if [ -f SumoLogicAWSObservabilityHelper.zip ]; then
+    rm SumoLogicAWSObservabilityHelper.zip
+fi
+
+if [ ! -f SumoLogicAWSObservabilityHelper.zip ]; then
+    echo "creating zip file"
+    mkdir python
+    cd python
+    pip install  crhelper -t .
+    pip install requests -t .
+    pip install boto3 -t .
+    cp -v ../../../sumologic-app-utils/src/*.py .
+    zip -r ../SumoLogicAWSObservabilityHelper.zip .
+    cd ..
+    rm -r python
+fi
