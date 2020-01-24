1d2b2b3f17b3aea4b080c0425f3ba7fd1175e89f
sourabh <sourabh@sumologic.com>
2020-01-24 22:31:02 +0530

    Testing for tags.

---
+++ sumologic-aws-observability/SupportingTemplates/TestTemplates/TestTagAWSResources.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+export AWS_REGION="us-east-1"
+export AWS_PROFILE="personal"
+# App to test
+export AppTemplateName="tag_resource"
+export AppName="tagresources"
+export InstallTypes=("rds" "alb" "apigateway" "ec2" "lambda")
+
+for InstallType in "${InstallTypes[@]}"
+do
+    if [[ "${InstallType}" == "rds" ]]
+    then
+        export AddTagsForALBResources="No"
+        export AddTagsForAPIGatewayResources="No"
+        export AddTagsForRDSResources="Yes"
+        export AddTagsForEC2MetricsResources="No"
+        export AddTagsForLambdaResources="No"
+    elif [[ "${InstallType}" == "alb" ]]
+    then
+        export AddTagsForALBResources="Yes"
+        export AddTagsForAPIGatewayResources="No"
+        export AddTagsForRDSResources="No"
+        export AddTagsForEC2MetricsResources="No"
+        export AddTagsForLambdaResources="No"
+    elif [[ "${InstallType}" == "apigateway" ]]
+    then
+        export AddTagsForALBResources="No"
+        export AddTagsForAPIGatewayResources="Yes"
+        export AddTagsForRDSResources="No"
+        export AddTagsForEC2MetricsResources="No"
+        export AddTagsForLambdaResources="No"
+    elif [[ "${InstallType}" == "ec2" ]]
+    then
+        export AddTagsForALBResources="No"
+        export AddTagsForAPIGatewayResources="No"
+        export AddTagsForRDSResources="No"
+        export AddTagsForEC2MetricsResources="Yes"
+        export AddTagsForLambdaResources="No"
+    elif [[ "${InstallType}" == "lambda" ]]
+    then
+        export AddTagsForALBResources="No"
+        export AddTagsForAPIGatewayResources="No"
+        export AddTagsForRDSResources="No"
+        export AddTagsForEC2MetricsResources="No"
+        export AddTagsForLambdaResources="Yes"
+    else
+        echo "No Choice"
+    fi
+
+    # Export Sumo Properties
+    export SumoAccessID=""
+    export SumoAccessKey=""
+    export SumoOrganizationId=""
+    export SumoDeployment="us1"
+    export RemoveSumoResourcesOnDeleteStack=true
+
+    # Export Tags Details
+    export AWSRegion="Current Region"
+    export AccountAlias="testing-tags"
+
+    export template_file="${AppTemplateName}.template.yaml"
+
+    aws cloudformation deploy --profile ${AWS_PROFILE} --template-file ././../${template_file} \
+    --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --stack-name "${AppName}-${InstallType}" \
+    --parameter-overrides SumoDeployment="${SumoDeployment}" SumoAccessID="${SumoAccessID}" SumoAccessKey="${SumoAccessKey}" \
+    SumoOrganizationId="${SumoOrganizationId}" RemoveSumoResourcesOnDeleteStack="${RemoveSumoResourcesOnDeleteStack}" \
+    AddTagsForALBResources="${AddTagsForALBResources}" AWSRegion="${AWSRegion}" AddTagsForAPIGatewayResources="${AddTagsForAPIGatewayResources}" \
+    AddTagsForRDSResources="${AddTagsForRDSResources}" AddTagsForEC2MetricsResources="${AddTagsForEC2MetricsResources}" \
+    AddTagsForLambdaResources="${AddTagsForLambdaResources}" AccountAlias="${AccountAlias}"
+
+done
+
+echo "All Installation Complete for ${AppName}"
