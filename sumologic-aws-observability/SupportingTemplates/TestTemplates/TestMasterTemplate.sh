1425fcc3e58a8773c136ac646af07fb5c0c61b76
sourabh <sourabh@sumologic.com>
2020-01-27 02:18:29 +0530

    test master

--- sumologic-aws-observability/SupportingTemplates/TestTemplates/TestMasterTemplate.sh
+++ sumologic-aws-observability/SupportingTemplates/TestTemplates/TestMasterTemplate.sh
@@ -5,34 +5,79 @@ export AWS_PROFILE="personal"
 # App to test
 export AppTemplateName="sumologic_observability.master"
 export AppName="master"
-export InstallTypes=("nothing" "explorer")
+# export InstallTypes=("nothing" "explorer")
+# export InstallTypes=("ec2app" "ec2withMeta")
+# export InstallTypes=("alldynamo" "dynamoapp" "dynamoallwithexistingbucket" "albapp")
+# export InstallTypes=("allalb")
+export InstallTypes=("alballwithexistingbucket")
+# export InstallTypes=("rdsapp" "rdscloudwatch" "allapi" "apiapp" "apiallwithexistingbucket")
 
 for InstallType in "${InstallTypes[@]}"
 do
+
     export AccountAlias="test-master-${InstallType}"
 
+    # Export Explorer Option
+    export ExplorerName="AWS Explorer Test View"
     export CreateExplorerView="No"
+
+    # EC2 App Configuration
     export InstallEC2App="No"
-    export InstallALBApp="No"
-    export InstallDynamoDBApp="No"
-    export InstallRDSApp="No"
-    export InstallLambdaApp="No"
-    export InstallAPIGatewayApp="No"
     export CreateMetaDataSource="No"
+    export Ec2AppSourceCategoryName="HostMetrics"
+
+    # ALB App Configuration
+    export InstallALBApp="No"
     export CreateS3Bucket="No"
+    export S3LogsBucketName="lambda-all-randmomstring"
     export CreateS3LogSource="No"
+    export AlbS3LogsCollectorName="alb-my-collector"
+    export AlbS3LogsSourceName="alb-my-source"
+    export AlbS3LogsSourceCategoryName="ALB/Existing/Logs"
     export CreateAlbCloudWatchMetricsSource="No"
+    export AlbCloudWatchMetricsSourceCategoryName="ALB/Existing/Metrics"
+
+    # Dynamo DB App Configuration
+    export InstallDynamoDBApp="No"
     export CreateDynamoDBCloudTrailBucket="No"
+    export DynamoDBCloudTrailLogsBucketName="lambda-all-randmomstring"
     export CreateDynamoDBCloudTrailLogSource="No"
+    export DynamoDBCloudTrailCollectorName="dynamo-my-collector"
+    export DynamoDBCloudTrailLogsSourceName="dynamo-my-source"
+    export DynamoDBCloudTrailLogsSourceCategoryName="Dynamo/Existing/Logs"
     export CreateDynamoDBCloudWatchMetricsSource="No"
+    export DynamoDBCloudWatchMetricsSourceCategoryName="Dynamo/Existing/Metrics"
+
+    # RDS App configuration
+    export InstallRDSApp="No"
     export CreateRdsCloudWatchMetricsSource="No"
+    export RdsCloudWatchMetricsSourceCategoryName="RDS/Existing/Metrics"
+
+    # Lambda App Configuration
+    export InstallLambdaApp="No"
     export CreateLambdaCloudTrailBucket="No"
+    export LambdaCloudTrailLogsBucketName
     export CreateLambdaCloudTrailLogSource="No"
+    export LambdaCloudTrailCollectorName
+    export LambdaCloudTrailLogsSourceName
+    export LambdaCloudTrailLogsSourceCategoryName
     export CreateLambdaCloudWatchMetricsSource="No"
+    export LambdaCloudWatchLogsCollectorName
+    export LambdaCloudWatchLogsSourceName
+    export LambdaCloudWatchLogsSourceCategoryName
     export CreateLambdaCloudWatchLogsSource="No"
+    export LambdaCloudWatchMetricsSourceCategoryName
+
+    # API Gateway App Configuration
+    export InstallAPIGatewayApp="No"
     export CreateApiGatewayCloudTrailBucket="No"
+    export ApiGatewayCloudTrailLogsBucketName="lambda-all-randmomstring"
     export CreateApiGatewayCloudTrailLogSource="No"
+    export ApiGatewayCloudTrailCollectorName="api-my-collector"
+    export ApiGatewayCloudTrailLogsSourceName="api-my-source"
+    export ApiGatewayCloudTrailLogsSourceCategoryName="Api/Existing/logs"
     export CreateApiGatewayCloudWatchMetricsSource="No"
+    export ApiGatewayCloudWatchMetricsSourceCategoryName="Api/Existing/Metrics"
 
     if [[ "${InstallType}" == "nothing" ]]
     then
@@ -40,6 +85,65 @@ do
     elif [[ "${InstallType}" == "explorer" ]]
     then
         export CreateExplorerView="Yes"
+    elif [[ "${InstallType}" == "ec2app" ]]
+    then
+        export InstallEC2App="Yes"
+    elif [[ "${InstallType}" == "ec2withMeta" ]]
+    then
+        export InstallEC2App="Yes"
+        export CreateMetaDataSource="Yes"
+    elif [[ "${InstallType}" == "albapp" ]]
+    then
+        export InstallALBApp="Yes"
+    elif [[ "${InstallType}" == "allalb" ]]
+    then
+        export InstallALBApp="Yes"
+        export CreateS3Bucket="Yes"
+        export CreateS3LogSource="Yes"
+        export CreateAlbCloudWatchMetricsSource="Yes"
+    elif [[ "${InstallType}" == "alballwithexistingbucket" ]]
+    then
+        export InstallALBApp="Yes"
+        export CreateS3Bucket="No"
+        export CreateS3LogSource="Yes"
+        export CreateAlbCloudWatchMetricsSource="Yes"
+    elif [[ "${InstallType}" == "dynamoapp" ]]
+    then
+        export InstallDynamoDBApp="Yes"
+    elif [[ "${InstallType}" == "alldynamo" ]]
+    then
+        export InstallDynamoDBApp="Yes"
+        export CreateDynamoDBCloudTrailBucket="Yes"
+        export CreateDynamoDBCloudTrailLogSource="Yes"
+        export CreateDynamoDBCloudWatchMetricsSource="Yes"
+    elif [[ "${InstallType}" == "dynamoallwithexistingbucket" ]]
+    then
+        export InstallDynamoDBApp="Yes"
+        export CreateDynamoDBCloudTrailBucket="No"
+        export CreateDynamoDBCloudTrailLogSource="Yes"
+        export CreateDynamoDBCloudWatchMetricsSource="No"
+    elif [[ "${InstallType}" == "rdsapp" ]]
+    then
+        export InstallRDSApp="Yes"
+    elif [[ "${InstallType}" == "rdscloudwatch" ]]
+    then
+        export InstallRDSApp="Yes"
+        export CreateRdsCloudWatchMetricsSource="Yes"
+    elif [[ "${InstallType}" == "apiapp" ]]
+    then
+        export InstallAPIGatewayApp="Yes"
+    elif [[ "${InstallType}" == "allapi" ]]
+    then
+        export InstallAPIGatewayApp="Yes"
+        export CreateApiGatewayCloudTrailBucket="Yes"
+        export CreateApiGatewayCloudTrailLogSource="Yes"
+        export CreateApiGatewayCloudWatchMetricsSource="Yes"
+    elif [[ "${InstallType}" == "apiallwithexistingbucket" ]]
+    then
+        export InstallAPIGatewayApp="Yes"
+        export CreateApiGatewayCloudTrailBucket="No"
+        export CreateApiGatewayCloudTrailLogSource="Yes"
+        export CreateApiGatewayCloudWatchMetricsSource="No"
     else
         echo "No Choice"
     fi
@@ -51,9 +155,6 @@ do
     export SumoDeployment="us1"
     export RemoveSumoResourcesOnDeleteStack=true
     export AWSRegion="Current Region"
-
-    # Export Explorer Option
-    export ExplorerName="AWS Explorer Test View"
 
     export template_file="${AppTemplateName}.template.yaml"
 
@@ -70,7 +171,15 @@ do
     CreateRdsCloudWatchMetricsSource="${CreateRdsCloudWatchMetricsSource}" CreateLambdaCloudTrailBucket="${CreateLambdaCloudTrailBucket}" \
     CreateLambdaCloudTrailLogSource="${CreateLambdaCloudTrailLogSource}" CreateLambdaCloudWatchMetricsSource="${CreateLambdaCloudWatchMetricsSource}" \
     CreateLambdaCloudWatchLogsSource="${CreateLambdaCloudWatchLogsSource}" CreateApiGatewayCloudTrailBucket="${CreateApiGatewayCloudTrailBucket}" \
-    CreateApiGatewayCloudTrailLogSource="${CreateApiGatewayCloudTrailLogSource}" CreateApiGatewayCloudWatchMetricsSource="${CreateApiGatewayCloudWatchMetricsSource}"
+    CreateApiGatewayCloudTrailLogSource="${CreateApiGatewayCloudTrailLogSource}" CreateApiGatewayCloudWatchMetricsSource="${CreateApiGatewayCloudWatchMetricsSource}" \
+    Ec2AppSourceCategoryName="${Ec2AppSourceCategoryName}" S3LogsBucketName="${S3LogsBucketName}" AlbS3LogsCollectorName="${AlbS3LogsCollectorName}" \
+    AlbS3LogsSourceName="${AlbS3LogsSourceName}" AlbS3LogsSourceCategoryName="${AlbS3LogsSourceCategoryName}" AlbCloudWatchMetricsSourceCategoryName="${AlbCloudWatchMetricsSourceCategoryName}" \
+    DynamoDBCloudTrailLogsBucketName="${DynamoDBCloudTrailLogsBucketName}" DynamoDBCloudTrailCollectorName="${DynamoDBCloudTrailCollectorName}" \
+    DynamoDBCloudTrailLogsSourceName="${DynamoDBCloudTrailLogsSourceName}" DynamoDBCloudTrailLogsSourceCategoryName="${DynamoDBCloudTrailLogsSourceCategoryName}" \
+    DynamoDBCloudWatchMetricsSourceCategoryName="${DynamoDBCloudWatchMetricsSourceCategoryName}" RdsCloudWatchMetricsSourceCategoryName="${RdsCloudWatchMetricsSourceCategoryName}" \
+    ApiGatewayCloudTrailLogsBucketName="${ApiGatewayCloudTrailLogsBucketName}" ApiGatewayCloudTrailCollectorName="${ApiGatewayCloudTrailCollectorName}" \
+    ApiGatewayCloudTrailLogsSourceName="${ApiGatewayCloudTrailLogsSourceName}" ApiGatewayCloudTrailLogsSourceCategoryName="${ApiGatewayCloudTrailLogsSourceCategoryName}" \
+    ApiGatewayCloudWatchMetricsSourceCategoryName="${ApiGatewayCloudWatchMetricsSourceCategoryName}"
 
 done
 
