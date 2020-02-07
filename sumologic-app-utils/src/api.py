cf902639c2a4e5fcc9dddb29e7824c882c22f82e
himanshu219 <hp.iiita@gmail.com>
2020-02-07 16:13:52 +0530

    adding mum region support

--- sumologic-app-utils/src/api.py
+++ sumologic-app-utils/src/api.py
@@ -146,7 +146,7 @@ class SumoResource(object):
     def api_endpoint(self):
         if self.deployment == "us1":
             return "https://api.sumologic.com/api"
-        elif self.deployment in ["ca", "au", "de", "eu", "jp", "us2"]:
+        elif self.deployment in ["ca", "au", "de", "eu", "jp", "us2", "fed", "in"]:
             return "https://api.%s.sumologic.com/api" % self.deployment
         else:
             return 'https://%s-api.sumologic.net/api' % self.deployment
