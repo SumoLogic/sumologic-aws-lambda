06deb69e77f4e6562c58d788ea9e90f5b701284b
sourabh <sourabh@sumologic.com>
2020-03-27 21:45:42 +0530

    Adding Bucket policy in case ALB logs bucket does not have required policy.

--- sumologic-app-utils/src/awsresource.py
+++ sumologic-app-utils/src/awsresource.py
@@ -1,4 +1,4 @@
-import importlib
+import json
 import os
 import re
 from abc import abstractmethod
@@ -167,7 +167,9 @@ class EnableS3LogsResources(AWSResource):
     def __init__(self, props, *args, **kwargs):
         print('Enabling S3 for ALB aws resource %s' % props.get("AWSResource"))
 
-    def _s3_logs_alb_resources(self, region_value, aws_resource, bucket_name, bucket_prefix, delete_flag, filter_regex):
+    def _s3_logs_alb_resources(self, region_value, aws_resource, bucket_name, bucket_prefix,
+                               delete_flag, filter_regex, region_account_id):
+
         # Get the class instance based on AWS Resource
         tag_resource = AWSResourcesProvider.get_provider(aws_resource, region_value, None)
 
@@ -183,24 +185,28 @@ class EnableS3LogsResources(AWSResource):
             if delete_flag:
                 tag_resource.disable_s3_logs(arns, bucket_name)
             else:
-                tag_resource.enable_s3_logs(arns, bucket_name, bucket_prefix)
+                tag_resource.enable_s3_logs(arns, bucket_name, bucket_prefix, region_account_id)
 
-    def create(self, region_value, aws_resource, bucket_name, bucket_prefix, filter_regex, *args, **kwargs):
+    def create(self, region_value, aws_resource, bucket_name, bucket_prefix, filter_regex, region_account_id,
+               *args, **kwargs):
         print("ENABLE S3 LOGS - Starting the AWS resources S3 addition to bucket %s." % bucket_name)
-        self._s3_logs_alb_resources(region_value, aws_resource, bucket_name, bucket_prefix, False, filter_regex)
+        self._s3_logs_alb_resources(region_value, aws_resource, bucket_name, bucket_prefix,
+                                    False, filter_regex, region_account_id)
         print("ENABLE S3 LOGS - Completed the AWS resources S3 addition to bucket.")
 
         return {"S3_ENABLE": "Successful"}, "S3"
 
-    def update(self, region_value, aws_resource, bucket_name, bucket_prefix, filter_regex, *args, **kwargs):
-        self.create(region_value, aws_resource, bucket_name, bucket_prefix, filter_regex, *args, **kwargs)
+    def update(self, region_value, aws_resource, bucket_name, bucket_prefix, filter_regex, region_account_id,
+               *args, **kwargs):
+        self.create(region_value, aws_resource, bucket_name, bucket_prefix, filter_regex, region_account_id,
+                    *args, **kwargs)
         print("updated S3 bucket to %s " % bucket_name)
         return {"S3_ENABLE": "Successful"}, "S3"
 
     def delete(self, region_value, aws_resource, bucket_name, bucket_prefix, filter_regex, remove_on_delete_stack,
                *args, **kwargs):
         if remove_on_delete_stack:
-            self._s3_logs_alb_resources(region_value, aws_resource, bucket_name, bucket_prefix, True, filter_regex)
+            self._s3_logs_alb_resources(region_value, aws_resource, bucket_name, bucket_prefix, True, filter_regex, "")
             print("ENABLE S3 LOGS - Completed the AWS resources S3 deletion to bucket.")
         else:
             print("ENABLE S3 LOGS - Skipping the AWS resources S3 deletion to bucket.")
@@ -213,6 +219,7 @@ class EnableS3LogsResources(AWSResource):
             "bucket_name": props.get("BucketName"),
             "bucket_prefix": props.get("BucketPrefix"),
             "filter_regex": props.get("Filter"),
+            "region_account_id": props.get("RegionAccountId"),
             "remove_on_delete_stack": props.get("RemoveOnDeleteStack")
         }
 
@@ -321,6 +328,7 @@ def enable_s3_logs(event, context):
     bucket_prefix = os.environ.get("BucketPrefix")
     account_id = os.environ.get("AccountID")
     filter_regex = os.environ.get("Filter")
+    region_account_id = os.environ.get("RegionAccountId")
 
     if "detail" in event:
         event_detail = event.get("detail")
@@ -336,7 +344,7 @@ def enable_s3_logs(event, context):
             resources = alb_resource.get_arn_list_cloud_trail_event(event_detail)
 
             # Enable S3 logging
-            alb_resource.enable_s3_logs(resources, bucket_name, bucket_prefix)
+            alb_resource.enable_s3_logs(resources, bucket_name, bucket_prefix, region_account_id)
 
     print("AWS S3 ENABLE ALB :- Completed s3 logs enable")
 
@@ -807,7 +815,7 @@ class AlbResources(AWSResourcesAbstract):
     def tag_resources_cloud_trail_event(self, arns, tags):
         self.client.add_tags(ResourceArns=arns, Tags=tags)
 
-    def enable_s3_logs(self, arns, s3_bucket, s3_prefix):
+    def enable_s3_logs(self, arns, s3_bucket, s3_prefix, elb_region_account_id):
         attributes = [{'Key': 'access_logs.s3.enabled', 'Value': 'true'},
                       {'Key': 'access_logs.s3.bucket', 'Value': s3_bucket},
                       {'Key': 'access_logs.s3.prefix', 'Value': s3_prefix}]
@@ -817,7 +825,45 @@ class AlbResources(AWSResourcesAbstract):
             if "Attributes" in response:
                 for attribute in response["Attributes"]:
                     if attribute["Key"] == "access_logs.s3.enabled" and attribute["Value"] == "false":
-                        self.client.modify_load_balancer_attributes(LoadBalancerArn=arn, Attributes=attributes)
+                        try:
+                            self.client.modify_load_balancer_attributes(LoadBalancerArn=arn, Attributes=attributes)
+                        except ClientError as e:
+                            if "Error" in e.response and "Message" in e.response["Error"] \
+                                    and "Access Denied for bucket" in e.response['Error']['Message']:
+                                self.add_bucket_policy(s3_bucket, elb_region_account_id)
+                                self.enable_s3_logs(arns, s3_bucket, s3_prefix, elb_region_account_id)
+                            else:
+                                raise e
+
+    def add_bucket_policy(self, bucket_name, elb_region_account_id):
+        print("Adding policy to the bucket " + bucket_name)
+        s3 = boto3.client('s3')
+        try:
+            response = s3.get_bucket_policy(Bucket=bucket_name)
+            existing_policy = json.loads(response["Policy"])
+        except ClientError as e:
+            if "Error" in e.response and "Code" in e.response["Error"] \
+                    and e.response['Error']['Code'] == "NoSuchBucketPolicy":
+                existing_policy = {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                    ]
+                }
+            else:
+                raise e
+
+        bucket_policy = {
+            'Sid': 'AwsAlbLogsasxasxa',
+            'Effect': 'Allow',
+            'Principal': {
+                "AWS": "arn:aws:iam::" + elb_region_account_id + ":root"
+            },
+            'Action': ['s3:PutObject'],
+            'Resource': f'arn:aws:s3:::{bucket_name}/*'
+        }
+        existing_policy["Statement"].append(bucket_policy)
+
+        s3.put_bucket_policy(Bucket=bucket_name, Policy=json.dumps(existing_policy))
 
     def disable_s3_logs(self, arns, s3_bucket):
         attributes = [{'Key': 'access_logs.s3.enabled', 'Value': 'false'}]
@@ -858,6 +904,9 @@ class AWSResourcesProvider(object):
 
 
 if __name__ == '__main__':
-    params = {}
-    value = ConfigDeliveryChannel()
-    value.create("Six_Hours", "config-bucket-668508221233", "config", "")
+    params = {"AWSResource": "elbv2"}
+    # value = ConfigDeliveryChannel()
+    # value.create("Six_Hours", "config-bucket-668508221233", "config", "")
+
+    value = EnableS3LogsResources(params)
+    value.create("us-east-1", "elbv2", "lambda-all-randmomstring", "elasticloadbalancing", "", "127311923021")
