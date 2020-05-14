e4063110f9a0b9a9f2dfd92c900485bd7b859470
sourabh <sourabh@sumologic.com>
2020-05-14 15:22:54 +0530

    Modifying DynamoDB Template with rearranging parameters, removing source category, adding existing resources tagging parameters question, removing role name because of update issue, added update source resource.

--- sumologic-aws-observability/apps/dynamodb/test/TestTemplate.sh
+++ sumologic-aws-observability/apps/dynamodb/test/TestTemplate.sh
@@ -18,25 +18,25 @@ export Section1eRemoveSumoResourcesOnDeleteStack=true
 export Section2bAccountAlias=${InstallType}
 export Section2cFilterExpression=".*"
 export Section4bCloudWatchMetricsSourceName="Source-metrics-${AppName}-${InstallType}"
-export Section5dS3BucketPathExpression="ALB_LOGS"
-export Section5fALBLogsSourceCategoryName="Labs/${AppName}/${InstallType}"
-export Section5bS3LogsBucketName="${AppName}-${InstallType}-${uid}"
+export Section5dCloudTrailBucketPathExpression="DYNAMODB_LOGS"
+export Section5fCloudTrailLogsSourceCategoryName="Labs/${AppName}/${InstallType}"
+export Section5bCloudTrailLogsBucketName="${AppName}-${InstallType}-${uid}"
 
 export Section2aTagExistingAWSResources="No"
 export Section3aInstallApp="No"
 export Section4aCreateCloudWatchMetricsSource="No"
-export Section5aCreateS3Bucket="No"
-export Section5cCreateALBLogSource="No"
+export Section5aCreateCloudTrailBucket="No"
+export Section5cCreateCloudTrailLogSource="No"
 
 if [[ "${InstallType}" == "all" ]]
 then
     export Section3bCollectorName="Sourabh-Collector-${AppName}-${InstallType}"
-    export Section5eALBLogsSourceName="Source-${AppName}-${InstallType}"
+    export Section5eCloudTrailLogsSourceName="Source-${AppName}-${InstallType}"
     export Section2aTagExistingAWSResources="Yes"
     export Section3aInstallApp="Yes"
     export Section4aCreateCloudWatchMetricsSource="Yes"
-    export Section5aCreateS3Bucket="Yes"
-    export Section5cCreateALBLogSource="Yes"
+    export Section5aCreateCloudTrailBucket="Yes"
+    export Section5cCreateCloudTrailLogSource="Yes"
 elif [[ "${InstallType}" == "onlyapp" ]]
 then
     export Section3aInstallApp="Yes"
@@ -50,23 +50,23 @@ then
 elif [[ "${InstallType}" == "onlylogsourcewithbucket" ]]
 then
     export Section3bCollectorName="Sourabh-Collector-${AppName}-${InstallType}"
-    export Section5eALBLogsSourceName="Source-${AppName}-${InstallType}"
-    export Section5aCreateS3Bucket="Yes"
-    export Section5cCreateALBLogSource="Yes"
+    export Section5eCloudTrailLogsSourceName="Source-${AppName}-${InstallType}"
+    export Section5aCreateCloudTrailBucket="Yes"
+    export Section5cCreateCloudTrailLogSource="Yes"
 elif [[ "${InstallType}" == "onlylogsourcewithoutbucket" ]]
 then
     export Section3bCollectorName="Sourabh-Collector-${AppName}-${InstallType}"
-    export Section5eALBLogsSourceName="Source-${AppName}-${InstallType}"
-    export Section5cCreateALBLogSource="Yes"
-    export Section5bS3LogsBucketName="sumologiclambdahelper-${AWS_REGION}"
+    export Section5eCloudTrailLogsSourceName="Source-${AppName}-${InstallType}"
+    export Section5cCreateCloudTrailLogSource="Yes"
+    export Section5bCloudTrailLogsBucketName="sumologiclambdahelper-${AWS_REGION}"
 elif [[ "${InstallType}" == "updatesourceonly" ]]
 then
-    export Section3bCollectorName="Sourabh-Collector-alb-onlylogsourcewithoutbucket"
-    export Section5eALBLogsSourceName="Source-alb-onlylogsourcewithoutbucket"
+    export Section3bCollectorName="Sourabh-Collector-${AppName}-onlylogsourcewithoutbucket"
+    export Section5eCloudTrailLogsSourceName="Source-${AppName}-onlylogsourcewithoutbucket"
 elif [[ "${InstallType}" == "nothing" ]]
 then
     export Section3bCollectorName=""
-    export Section5eALBLogsSourceName=""
+    export Section5eCloudTrailLogsSourceName=""
 else
     echo "No Valid Choice."
 fi
@@ -74,16 +74,14 @@ fi
 # Stack Name
 export stackName="${AppName}-${InstallType}"
 pwd
-aws cloudformation deploy --profile ${AWS_PROFILE} --template-file ./apps/${AppName}/alb_app.template.yaml --region ${AWS_REGION} \
+aws cloudformation deploy --profile ${AWS_PROFILE} --template-file ./apps/${AppName}/dynamodb_app.template.yaml --region ${AWS_REGION} \
 --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --stack-name ${stackName} \
 --parameter-overrides Section1aSumoDeployment="${Section1aSumoDeployment}" Section1bSumoAccessID="${Section1bSumoAccessID}" \
 Section1cSumoAccessKey="${Section1cSumoAccessKey}" Section1dSumoOrganizationId="${Section1dSumoOrganizationId}" \
 Section1eRemoveSumoResourcesOnDeleteStack="${Section1eRemoveSumoResourcesOnDeleteStack}" Section2bAccountAlias="${Section2bAccountAlias}" \
 Section2cFilterExpression="${Section2cFilterExpression}" Section3bCollectorName="${Section3bCollectorName}" \
-Section4bCloudWatchMetricsSourceName="${Section4bCloudWatchMetricsSourceName}" Section5dS3BucketPathExpression="${Section5dS3BucketPathExpression}" \
-Section5eALBLogsSourceName="${Section5eALBLogsSourceName}" Section5fALBLogsSourceCategoryName="${Section5fALBLogsSourceCategoryName}" \
+Section4bCloudWatchMetricsSourceName="${Section4bCloudWatchMetricsSourceName}" Section5dCloudTrailBucketPathExpression="${Section5dCloudTrailBucketPathExpression}" \
+Section5eCloudTrailLogsSourceName="${Section5eCloudTrailLogsSourceName}" Section5fCloudTrailLogsSourceCategoryName="${Section5fCloudTrailLogsSourceCategoryName}" \
 Section2aTagExistingAWSResources="${Section2aTagExistingAWSResources}" Section3aInstallApp="${Section3aInstallApp}" \
-Section4aCreateCloudWatchMetricsSource="${Section4aCreateCloudWatchMetricsSource}" Section5aCreateS3Bucket="${Section5aCreateS3Bucket}" \
-Section5cCreateALBLogSource="${Section5cCreateALBLogSource}" Section5bS3LogsBucketName="${Section5bS3LogsBucketName}"
-
-
+Section4aCreateCloudWatchMetricsSource="${Section4aCreateCloudWatchMetricsSource}" Section5aCreateCloudTrailBucket="${Section5aCreateCloudTrailBucket}" \
+Section5cCreateCloudTrailLogSource="${Section5cCreateCloudTrailLogSource}" Section5bCloudTrailLogsBucketName="${Section5bCloudTrailLogsBucketName}"
--- sumologic-aws-observability/apps/ec2metrics/test/TestTemplate.sh
+++ sumologic-aws-observability/apps/ec2metrics/test/TestTemplate.sh
@@ -50,7 +50,7 @@ fi
 # Stack Name
 export stackName="${AppName}-${InstallType}"
 pwd
-aws cloudformation deploy --profile ${AWS_PROFILE} --template-file ./${AppName}/ec2_metrics_app.template.yaml --region ${AWS_REGION} \
+aws cloudformation deploy --profile ${AWS_PROFILE} --template-file ./apps/${AppName}/ec2_metrics_app.template.yaml --region ${AWS_REGION} \
 --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --stack-name ${stackName} \
 --parameter-overrides Section1aSumoDeployment="${Section1aSumoDeployment}" Section1bSumoAccessID="${Section1bSumoAccessID}" \
 Section1cSumoAccessKey="${Section1cSumoAccessKey}" Section1dSumoOrganizationId="${Section1dSumoOrganizationId}" \
