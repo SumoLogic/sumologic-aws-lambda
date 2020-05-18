c1d0174e353075c06f467bae2006def3cc06a92a
sourabh <sourabh@sumologic.com>
2020-05-18 18:29:52 +0530

    auto enable s3 logging

--- sumologic-aws-observability/apps/AutoEnableS3LogsAlb/test/TestTemplate.sh
+++ sumologic-aws-observability/apps/AutoEnableS3LogsAlb/test/TestTemplate.sh
@@ -9,8 +9,6 @@ export InstallTypes=("s3" "s3exiting" "vpc" "vpcexisting" "alb" "albexisting")
 export BucketName="sumologiclambdahelper-${AWS_REGION}"
 export FilterExpression=".*"
 
-export ExistingResource="No"
-
 for InstallType in "${InstallTypes[@]}"
 do
     export BucketPrefix=${InstallType}"-LOGS/"
@@ -18,24 +16,27 @@ do
     if [[ "${InstallType}" == "s3" ]]
     then
         export EnableLogging="S3"
+        export TaggingResourceOptions="New"
     elif [[ "${InstallType}" == "s3exiting" ]]
     then
         export EnableLogging="S3"
-        export ExistingResource="Yes"
+        export TaggingResourceOptions="Existing"
     elif [[ "${InstallType}" == "vpc" ]]
     then
         export EnableLogging="VPC"
+        export TaggingResourceOptions="New"
     elif [[ "${InstallType}" == "vpcexisting" ]]
     then
         export EnableLogging="VPC"
-        export ExistingResource="Yes"
+        export TaggingResourceOptions="Existing"
     elif [[ "${InstallType}" == "alb" ]]
     then
         export EnableLogging="ALB"
+        export TaggingResourceOptions="New"
     elif [[ "${InstallType}" == "albexisting" ]]
     then
         export EnableLogging="ALB"
-        export ExistingResource="Yes"
+        export TaggingResourceOptions="Existing"
         export BucketPrefix=${InstallType}"-LOGS"
     else
         echo "No Valid Choice."
@@ -46,7 +47,7 @@ do
 
     aws cloudformation deploy --region ${AWS_REGION} --profile ${AWS_PROFILE} --template-file ././../auto_enable_s3_alb.template.yaml \
     --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM --stack-name "${AppName}-${InstallType}" \
-    --parameter-overrides EnableLogging="${EnableLogging}" ExistingResource="${ExistingResource}" \
+    --parameter-overrides EnableLogging="${EnableLogging}" TaggingResourceOptions="${TaggingResourceOptions}" \
     FilterExpression="${FilterExpression}" BucketName="${BucketName}" BucketPrefix="${BucketPrefix}" &
 
     export ExistingResource="No"
