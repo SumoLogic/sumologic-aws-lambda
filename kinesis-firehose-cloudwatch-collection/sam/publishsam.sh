f2bac6458b6b2a58265e2c300454477ebb2ed2b9
sourabh <sourabh@sumologic.com>
2021-04-15 08:50:43 +0530

    Updating metrics template to provide namespace filtering.

--- kinesis-firehose-cloudwatch-collection/sam/publishsam.sh
+++ kinesis-firehose-cloudwatch-collection/sam/publishsam.sh
@@ -17,30 +17,36 @@ app_names=(
     "KinesisFirehoseCWLogs:logs" "KinesisFirehoseCWMetrics:metrics"
 )
 
-echo `sam --version`
+sam --version
+
 # Regex to deploy only expected templates.
-match_case="KinesisFirehose"
+match_case="KinesisFirehoseCWMetrics"
 
 for app_name in "${app_names[@]}"
 do
     KEY="${app_name%%:*}"
     VALUE="${app_name##*:}"
 
-    template_path="${VALUE}/${KEY}.template.yaml"
-    packaged_path="${VALUE}/packaged.yaml"
+    template_path="${KEY}.template.yaml"
+    packaged_path="packaged.yaml"
 
     if [[ "${KEY}" == *"${match_case}"* ]]; then
         # Grep Version from the SAM Template.
-        export version=`grep AWS::ServerlessRepo::Application: ../${template_path} -A 20 | grep SemanticVersion | cut -d ':' -f 2 | xargs`
+        cd ../"${VALUE}" || exit
+
+        export version=`grep AWS::ServerlessRepo::Application: ${template_path} -A 20 | grep SemanticVersion | cut -d ':' -f 2 | xargs`
         echo "Package and publish the Template file ${KEY} with version ${version}."
 
-        echo `sam validate -t ../${template_path}`
+        sam validate -t ${template_path}
 
-        sam package --profile ${AWS_PROFILE} --template-file ../${template_path} --s3-bucket ${SAM_S3_BUCKET} --output-template-file ../${packaged_path} \
+        sam build -t ${template_path}
+
+        sam package --profile ${AWS_PROFILE} --template-file .aws-sam/build/template.yaml --s3-bucket ${SAM_S3_BUCKET} --output-template-file ${packaged_path} \
         --s3-prefix "${KEY}/v${version}"
 
-        sam publish --template ../${packaged_path} --region ${AWS_REGION} --semantic-version ${version} --profile ${AWS_PROFILE}
+        sam publish --template ${packaged_path} --region ${AWS_REGION} --semantic-version ${version} --profile ${AWS_PROFILE}
         echo "Publish done"
     fi
+    cd ../sam || exit
 done
 
