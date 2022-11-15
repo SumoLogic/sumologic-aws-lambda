2f5f6db310014652856b5586341f3c09232270a7
Himanshu Sharma <hsharma@sumologic.com>
2022-11-15 22:16:20 +0530

    Updating security hub layers in new aws regions

--- securityhub-org/sumologic-securityhub-collector-awsorg/sam/create_layer.sh
+++ securityhub-org/sumologic-securityhub-collector-awsorg/sam/create_layer.sh
@@ -11,6 +11,8 @@ fi
 
 declare -a regions=("us-east-2" "us-east-1" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1")
 
+# Buckets names that are different from region name (e.g appdevzipfiles-eu-north-1s) - eu-north-1 , ap-east-1, af-south-1, me-south-1
+
 for i in "${regions[@]}"
 do
     echo "Deploying layer in $i"
