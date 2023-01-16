efd2295f388b8b447d0f0e2eafe17a372735354d
Himanshu Sharma <hsharma@sumologic.com>
2023-01-16 17:48:04 +0530

    Adding guardduty deploy scripts

---
+++ cloudwatchevents/guarddutybenchmark/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export AWS_PROFILE="prod"
+if [ "$AWS_PROFILE" == "prod" ]
+then
+    SAM_S3_BUCKET="appdevstore"
+    AWS_REGION="us-east-1"
+else
+    SAM_S3_BUCKET="cf-templates-5d0x5unchag-us-east-2"
+    AWS_REGION="us-east-2"
+fi
+
+version="1.0.12"
+
+echo "Creating package.yaml"
+sam package --template-file template_v2.yaml --s3-bucket $SAM_S3_BUCKET  --output-template-file packaged.yaml --s3-prefix "guarddutybenchmark/v"$version --region $AWS_REGION --profile $AWS_PROFILE
+
+echo "Publishing sumologic-guardduty-benchmark "$version
+sam publish --template packaged.yaml --region $AWS_REGION --semantic-version $version
+
+echo "Published sumologic-guardduty-benchmark "$version
+# sam deploy --template-file packaged_sumo_app_utils.yaml --stack-name testingsumoapputils --capabilities CAPABILITY_IAM --region $AWS_REGION
