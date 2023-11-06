f8ecd7617160b0fa347cb5b0c4964e15016ed3f3
project <langlq@gmail.com>
2023-11-06 11:25:37 +0700

    update lib and python

--- securityhub-org/sumologic-securityhub-collector-awsorg/sam/sam_package.sh
+++ securityhub-org/sumologic-securityhub-collector-awsorg/sam/sam_package.sh
@@ -3,21 +3,14 @@ then
     SAM_S3_BUCKET="appdevstore"
     AWS_REGION="us-east-1"
 else
-    SAM_S3_BUCKET="appdevstore20211221-prod"
+    SAM_S3_BUCKET="appstore-20231030-securityhub-collector-awsorg"
     AWS_REGION="us-east-1"
 fi
+sam package --template-file template.yaml --s3-bucket $SAM_S3_BUCKET  --output-template-file packaged.yaml
 
-version="1.0.3"
+sam publish --template packaged.yaml --region us-east-1
 
-echo "Creating package.yaml"
-sam package --template-file template.yaml --s3-bucket $SAM_S3_BUCKET  --output-template-file packaged.yaml --s3-prefix "SecurityHubCollectorAWSOrg/v"$version --region $AWS_REGION --profile $AWS_PROFILE
-
-echo "Publishing sumologic-securityhub-forwarder "$version
-sam publish --template packaged.yaml --region $AWS_REGION --semantic-version $version
-
-echo "Published sumologic-securityhub-forwarder "$version
-
-# sam deploy --template-file packaged.yaml --stack-name testingsechubawsorg --capabilities CAPABILITY_IAM --region $AWS_REGION --parameter-overrides ParameterKey=SumoEndpoint,ParameterValue=https://collectors.sumologic.com/receiver/v1/http/ZaVnC4dhaV29FhnR-VQyA9mpray7QOE0aRQrtZnuNmMQ0DKr9ZVMGY5WIa0IWSjt_LkiUSjI71WGiDHRHStqwCApBp_49e_W-b6gM0_KnZlxBUBe-1yTFw==
+# sam deploy --template-file packaged.yaml --stack-name testingsecurityhubcollectorawsorg --capabilities CAPABILITY_IAM --region $AWS_REGION --parameter-overrides ParameterKey=SumoEndpoint,ParameterValue=https://endpoint6.collection.us2.sumologic.com/receiver/v1/http/
 
 #aws --profile awsorg cloudformation describe-stack-events --stack-name testingsecurityhubcollectorawsorg --region $AWS_REGION
 #aws --profile awsorg cloudformation get-template --stack-name testingsecurityhubcollectorawsorg  --region $AWS_REGION
