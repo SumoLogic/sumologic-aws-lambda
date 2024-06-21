f3ce5bc44cade046ab4bb91ed319b4fb714dbba6
Himanshu Sharma <hsharma@sumologic.com>
2024-06-21 18:17:02 +0530

    fixed script

--- cloudformation-telemetry/deploy.sh
+++ cloudformation-telemetry/deploy.sh
@@ -4,7 +4,7 @@ rm src/external/*.pyc
 rm src/*.pyc
 rm telemetry.zip
 
-if [ ! -f sumo_app_utils.zip ]; then
+if [ ! -f telemetry.zip ]; then
     echo "creating zip file"
     mkdir python
     cd python
