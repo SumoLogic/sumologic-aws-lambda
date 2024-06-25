337502175ddab5e46b033c211d9a8f9b3a16bd29
Himanshu Sharma <hsharma@sumologic.com>
2024-06-25 21:17:27 +0530

    Updated deploy script

--- cloudformation-telemetry/deploy.sh
+++ cloudformation-telemetry/deploy.sh
@@ -9,9 +9,10 @@ if [ ! -f telemetry.zip ]; then
     mkdir python
     cd python
     pip3 install  crhelper -t .
-    pip3 install requests -t .
-    pip3 install retrying -t .
-    cp -v ../*.py .
+    pip3 install sumologic-appclient-sdk -t .
+    pip3 install future_fstrings -t .
+    pip3 install setuptools -t .
+    cp -v ../* .
     zip -r ../telemetry.zip .
     cd ..
     rm -r python
